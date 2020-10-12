fbc3cc0254ed4cbb4cb9745c48a8d2a205a31a03
Anni Mari Prees <anpree@ttu.ee>
2020-10-12 15:10:46 +0300

    Add project files.

---
+++ Blazor.Learner/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blazor.Learner.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blazor.Learner.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Developer> Developers { get; set; }
+    }
+}
